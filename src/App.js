56716bc833c1ea86e4cca0a14940f27080ef64df
Matheus <mateusmasi@gmail.com>
2024-05-13 16:34:15 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import Search from './Search';
 import './App.css';
 
+class App extends React.Component {
+  componentDidMount() {
+    // Add CSP headers dynamically
+    const meta = document.createElement('meta');
+    meta.httpEquiv = 'Content-Security-Policy';
+    meta.content = `
+      connect-src
+        api.scryfall.com
+        embed.scryfall.com;
+      img-src
+        *.scryfall.io
+      style-src
+        embed.scryfall.com;
+      script-src
+        embed.scryfall.com;
+      font-src
+        embed.scryfall.com;
+    `;
+    document.head.appendChild(meta);
+  }
 
-const App = () => {
-  return (
-    <div className="App">
-      <Search />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        {/* Your app content goes here */}
+      </div>
+    );
+  }
+}
 
 export default App;
