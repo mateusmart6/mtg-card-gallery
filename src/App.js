25b1fd26093f1c3d26006af0bcb1d8f12703d04b
Matheus <mateusmasi@gmail.com>
2024-05-13 19:02:00 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import Search from './Search';
-import Home from './Home';
-import Start from './Start';
-import About from './About';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="startcreating" element={<Start />} />
-          <Route exact path="aboutus" element={<About />} />
-          {/* <Route path="signup" element={<SignUpModel />} />  (if needed) */}
-        </Routes>
-        <Search />  {/* Assuming you want the Search component to be rendered always */}
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Search />
+    </div>
   );
-}
+};
 
 export default App;
